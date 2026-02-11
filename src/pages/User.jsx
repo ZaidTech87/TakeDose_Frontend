f22064adedba425c4dd2449475feb018570f458d
Mohd Zaid <zaidalig1234@gmail.com>
2026-02-11 23:07:08 +0530

    Create User page component

---
+++ src/pages/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div>
+            <h1>User Page</h1>
+            <p>This is the user page component.</p>
+        </div>
+    );
+};
+
+export default User;
